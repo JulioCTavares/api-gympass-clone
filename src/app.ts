12bc6a337ce5749e83f78b1cd36289155d1eb91e
Julio Tavares <juliotavares197@gmail.com>
2023-11-22 20:19:44 -0300

    feat: global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
 import fastify from 'fastify';
 import { appRoutes } from './http';
+import { ZodError } from 'zod';
+import { env } from './env';
 
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, req, reply) => {
+	if (error instanceof ZodError) {
+		return reply.status(400).send({ message: 'Validation error', issue: error.format()});
+	}
+
+	if (env.NODE_ENV !== 'production') {
+		console.error(error);
+	}
+
+	return reply.status(500).send({ message: 'Internal Server Error'});
+});
